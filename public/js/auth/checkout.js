3d85ec500e05509cbddc4da2f039f155bed2f6de
Joe Nesh <joemunene@protonmail.com>
2025-12-18 04:17:12 -0800

    h

--- public/js/auth/checkout.js
+++ public/js/auth/checkout.js
@@ -111,9 +111,11 @@ function showNotification() {
 			setTimeout(() => { document.getElementsByClassName('toast')[0].classList.add(`anon`); }, 200);	
 			var shortCutFunction = 'success'; var msg = `Use a burner email, <br> to complete the purchase. <hr class="to-hr hr15-top">`;
 			toastr.options =  { closeButton: true, debug: false, newestOnTop: true, timeOut: 3000,progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null }; var $toast = toastr[shortCutFunction](msg); $toastlast = $toast;
-			
+
 			if(window.innerWidth < 700) {
-				$("html, body").animate({ scrollTop: 630 }, 4000); 
+				setTimeout(() => {
+					$("html, body").animate({ scrollTop: 630 }, 3000); 
+				}, 600);
 			}
 		}, 2000);
 	});
